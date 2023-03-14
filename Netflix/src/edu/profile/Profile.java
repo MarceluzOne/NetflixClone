ebab7a474d320b2c935ae91c2f63d9c8e62de889
Marcelo Arruda <arruda16.marcelo@gmail.com>
2023-03-14 15:41:25 -0300

    Correção profile

--- Netflix/src/edu/profile/Profile.java
+++ Netflix/src/edu/profile/Profile.java
@@ -5,42 +5,43 @@ public class Profile {
   String name;
   int age;
   String cpf;
-  String adress;
-  String gender;
+  String email;
+  String telefone;
 
   public Profile(
     String name,
     int age,
     String cpf,
-    String adress,
-    String gender){
+    String email,
+    String telefone){
       this.name = name;
       this.age = age;
+      this.cpf = cpf;
+      this.email = email;
+      this.telefone = telefone;
   }
-
-  public static void main(String[] args) {
+  public String toString()
+	{
+		return (this.name + " " + this.age + " " + this.cpf + " " + this.email + " " + this.telefone);
+	}
+  public static void cadastro(){
+    
     Scanner text = new Scanner(System.in);
 
-    System.out.println("Qual o nome?");
-    String name = text.nextLine();
-    System.out.println("Qual a idade");
-    int age = text.nextInt();
     System.out.println("Qual o CPF");
     String cpf = text.nextLine();
-    System.out.println("Qual o Endereço");
-    String adress =text.nextLine();
-    System.out.println("Qual o genero");
-    String gender = text.nextLine();
-
-    Profile pessoa = new Profile(name,age,cpf,adress,gender);
-    
-    System.out.println();
-
-
-
-
-
-
-
+    System.out.println("Qual o nome?");
+    String name = text.nextLine();
+    System.out.println("Qual o email");
+    String email =text.nextLine();
+    System.out.println("Qual o telefone");
+    String telefone = text.nextLine();
+    System.out.println("Qual a idade");
+    int age = text.nextInt();
+    Profile pessoa = new Profile(name,age,cpf,email,telefone);
+    System.out.println(pessoa);
+  }
+  public static void main(String[] args) {
+    cadastro();
   }
 }
