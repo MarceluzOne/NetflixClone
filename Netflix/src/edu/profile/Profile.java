27529a345d6ec583c9f616880712ab1cdd0a033a
Marcelo Arruda <arruda16.marcelo@gmail.com>
2023-03-15 15:41:07 -0300

    Criação dos metodos Get e Set

--- Netflix/src/edu/profile/Profile.java
+++ Netflix/src/edu/profile/Profile.java
@@ -6,23 +6,57 @@ public class Profile {
   int age;
   String cpf;
   String email;
-  String telefone;
+  String phone;
 
   public Profile(
     String name,
     int age,
     String cpf,
     String email,
-    String telefone){
+    String phone){
       this.name = name;
       this.age = age;
       this.cpf = cpf;
       this.email = email;
-      this.telefone = telefone;
+      this.phone = phone;
   }
+
+  public String getName(){
+    return name;
+  }
+  public void setName(String name){
+    this.name = name;
+  }
+
+  public int getAge(){
+    return age;
+  }
+  public void setAge(int age){
+  this.age = age;
+  }
+
+  public String getCPF(){
+    return cpf;
+  }
+  public void setCPF(String cpf){
+    this.cpf = cpf;
+  }
+  public String getEmail(){
+    return email;
+  }
+  public void setEmail(String email){
+    this.email = email;
+  }
+  public String getPhone(){
+    return phone;
+  }
+  public void setPhone(String phone){
+    this.phone = phone;
+  }
+
   public String toString()
 	{
-		return ("O Nome é: " + this.name + "\nA Idade é: " + this.age + "\nO CPF é:  " + this.cpf + "\nO E-mail é: " + this.email + "\nO telefone é: " + this.telefone);
+		return ("O Nome é: " + this.name + "\nA Idade é: " + this.age + "\nO CPF é:  " + this.cpf + "\nO E-mail é: " + this.email + "\nO telefone é: " + this.phone);
 	}
   public static void cadastro(){
     
@@ -35,10 +69,10 @@ public class Profile {
     System.out.println("Qual o email");
     String email =text.nextLine();
     System.out.println("Qual o telefone");
-    String telefone = text.nextLine();
+    String phone = text.nextLine();
     System.out.println("Qual a idade");
     int age = text.nextInt();
-    Profile pessoa = new Profile(name,age,cpf,email,telefone);
+    Profile pessoa = new Profile(name,age,cpf,email,phone);
     System.out.println(pessoa);
   }
   public static void main(String[] args) {
