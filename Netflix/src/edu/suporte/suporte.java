bbb1f930e6c6327bd5924b45f66c90b80822e786
Thialy Lima <thialys.lima@unifacol.edu.br>
2023-03-16 02:01:16 -0300

    Criando suporte fase1

--- Netflix/src/edu/suporte/suporte.java
+++ Netflix/src/edu/suporte/suporte.java
@@ -1,7 +1,41 @@
 package edu.suporte;
+import java.util.Scanner;
+
 
 public class suporte {
-  String suporte;
-  String teste = "Testando github";
-  String teste2 = "Consegui?";
+  String SolicitarTitulo;
+  String TermosDeUso;
+  String dicas;
+  String sobreNetflix;
+  String SAC;
+
+public suporte(
+  String SolicitarTitulo,
+  String TermosDeUso,
+  String dicas,
+  String sobreNetflix,
+  String SAC) {
+    this.SolicitarTitulo = SolicitarTitulo;
+    this.TermosDeUso = TermosDeUso;
+    this.dicas = dicas;
+    this.sobreNetflix= sobreNetflix;
+    this.SAC = SAC;
+  }
+/* 
+public static void escolha(){
+
+  Scanner text = new Scanner(System.in);
+  
+  System.out.println("Qual opção gostaria de acessar?");
+  System.out.println("[1]Solicitar Título\n[2]Termos de Uso\n[3]Dicas Netflix\n[4]Sobre a Netflix\n[5]SAC");
+  
+    System.out.println("Qual título deseja solicitar? ");
+    String option = text.nextLine();
+
+  if (option == "1"){
+    System.out.println("Título escolhido: " + titulo);
+    suporte titulo = new suporte(SolicitarTitulo);
+    System.out.println(titulo);
+  }
+}*/
 }
