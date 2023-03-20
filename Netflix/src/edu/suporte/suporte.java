3856bd913b3d74a13e23a040844ad74b4971a730
Thialy Lima <thialys.lima@unifacol.edu.br>
2023-03-20 15:43:08 -0300

    Corrigindo erros

--- Netflix/src/edu/suporte/suporte.java
+++ Netflix/src/edu/suporte/suporte.java
@@ -58,7 +58,7 @@ public class Suporte {
     this.SolicitarTitulo = titulo;
   }
   //método
-  public static void Suporte(){
+  public static void suporte(){
     Scanner text = new Scanner(System.in);
 
     String SolicitarTitulo = " ";
@@ -67,56 +67,63 @@ public class Suporte {
     String sobreNetflix = ("\nSOBRE A NETFLIX: A Netflix é um serviço de streaming que oferece uma ampla variedade de \nséries, filmes e documentários premiados em milhares de aparelhos conectados à internet.\nVocê pode assistir a quantos filmes e séries quiser, quando e onde quiser tudo por um preço mensal acessível.\n");
     String SAC = ("\nSAC: Entre em contato conosco: +55 (xx) x xxxx-xxxx \n");
     
-   
-    while (true){
-      Scanner scan = new Scanner(System.in);
+    Scanner scan = new Scanner(System.in);
+    int option;
 
+    do{
+    
       System.out.println("\n--------------SUPORTE--------------\nBem vindo(a) ao Suporte Netflix!\n[1]Solicitar um novo filme/título\n[2]Termos de Uso\n[3]Dicas Netflix\n[4]Sobre a Netflix\n[5]SAC\n[6]Sair\n");
       System.out.println("\nSUA OPÇÃO: \n");
-      int option = scan.nextInt();
-      
-      if (option == 6){
-        System.out.println("--SUPORTE FECHADO--");
-        scan.close();
-        break;
-      }
-      
+      option = scan.nextInt();
+
       switch(option){
-        case 1 : {
+
+        case 1 : 
           
           System.out.println("\nQual novo filme deseja solicitar?");
           SolicitarTitulo = text.nextLine();
           System.out.println("Título selecionado: " + SolicitarTitulo +" --- Obrigada pela sugestão!");
           
           break;
-        }
        
-        case 2: {
+        case 2: 
           System.out.println(TermosDeUso);
           break;
-        }
-        case 3: {
+        
+        case 3: 
           System.out.println(dicas);
           break;
-        }
-        case 4: {
+        
+        case 4: 
           System.out.println(sobreNetflix);
           break;
-        }
-        case 5: {
+        
+        case 5: 
           System.out.println(SAC);
           break;
-        }
-        default :{
+
+        case 6 :
+        System.out.println("----SUPORTE FECHADO----");
+        scan.close();
+        break;
+
+        default :
           System.out.println("Opção inválida!");
-        }
       }
-    }        
-    Suporte SUPORTE = new Suporte(SolicitarTitulo,TermosDeUso,dicas,sobreNetflix,SAC);
-  
-  }
+    } 
+    while (option != 6);
+      scan.close();
     
+    //guardando as info
+    Suporte SUPORTE = new Suporte(SolicitarTitulo,TermosDeUso,dicas,sobreNetflix,SAC);
+   
+    //System.out.println(SUPORTE);
+  }
+  //testando
+   /*public String toString(){
+      return("Titulo: " + this.SolicitarTitulo);}*/
+
   public static void main(String []args) {
-    Suporte();
+    suporte();
       }
 }
