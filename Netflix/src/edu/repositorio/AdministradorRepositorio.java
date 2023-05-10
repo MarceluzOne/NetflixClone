7e598072635c557043853f320a54226ad298594e
Marcelo Arruda <arruda16.marcelo@gmail.com>
2023-05-10 10:02:54 -0300

    Correções

--- Netflix/src/edu/repositorio/AdministradorRepositorio.java
+++ Netflix/src/edu/repositorio/AdministradorRepositorio.java
@@ -5,28 +5,28 @@ import java.util.ArrayList;
 import edu.entidades.Administrador;
 
 public class AdministradorRepositorio implements IAdministrador {
-    public ArrayList<Administrador> listaAdministrador = new ArrayList<Administrador>();
+	public ArrayList<Administrador> listaAdministrador = new ArrayList<Administrador>();
 
-    public boolean cadastrarAdministrador(Administrador name){
-        listaAdministrador.add(name);
-        return true;
-    }
-    
-    public ArrayList<Administrador> listarAdministrador(){
-        return listaAdministrador;
-    }
-    public boolean atualizarAdministrador(Administrador name){
-        return true;
-    }
-    public boolean validarAdministrador(ArrayList<Administrador> listaAdministrador, Administrador name){
-        return true;
-    }
-    @Override
-    public boolean deletarAdministrador(Administrador name) {
-      listaAdministrador.remove(name);
-      return true;
-    }
-    
+	public boolean cadastrarAdministrador(Administrador name){
+		listaAdministrador.add(name);
+		return true;
+	}
+	
+	public ArrayList<Administrador> listarAdministrador(){
+		return listaAdministrador;
+	}
+	public boolean atualizarAdministrador(Administrador name){
+		return true;
+	}
+	public boolean validarAdministrador(ArrayList<Administrador> listaAdministrador, Administrador name){
+		return true;
+	}
+	@Override
+	public boolean deletarAdministrador(Administrador name) {
+		listaAdministrador.remove(name);
+		return true;
+	}
+	
 
 
 
