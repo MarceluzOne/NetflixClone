f5ce48793af55c0f4b2627bc6884d136ced13d19
Thialy Lima <thialys.lima@unifacol.edu.br>
2023-05-17 20:04:58 -0300

    Atuallizações de serviços

--- Netflix/src/edu/entidades/Assinatura.java
+++ Netflix/src/edu/entidades/Assinatura.java
@@ -6,38 +6,18 @@ public class Assinatura  {
     private String formaPagamento;
 	private String dados;
 
-	public Assinatura(String plano, String formaPagamento) {
+	public Assinatura(String plano, String formaPagamento, String dados) {
         this.plano = plano;
         this.formaPagamento = formaPagamento;
 		this.dados = dados;
 	}
 
-	public String getDataPagamento(){
-    return dataPagamento;
-  }
-  public void setDataPagamento(String dataPagamento){
-    this.dataPagamento = dataPagamento;
-  }
-	public String getPlano(){
+	public String getplano(){
     return plano;
   }
-  public void setPlano(String plano){
-    this.plano = plano;
-  }
-
-	public String getformaPagamento() {
-		return formaPagamento;
-	}
 
 	public void setplano(String plano) {
 		this.plano = plano;
-	public void cadastroAssinaturaCredito(){
-		Scanner input = new Scanner(System.in);
-		System.out.println("Insira dos dados do cartão a ser cadastrado: ");
-		String cartao =input.nextLine();
-		System.out.println("Informe a melhor data de Pagamento");
-		String dataPagamento = input.nextLine();
-		System.out.println("O cartão inserido foi: " + cartao + "\nA melhor data de pagamento escolhida foi: " + dataPagamento);
 	}
 
 	public String getformaPagamento() {
@@ -47,7 +27,7 @@ public class Assinatura  {
 	public void setformaPagamento(String formaPagamento) {
 		this.formaPagamento = formaPagamento;
 	}
-
+	
 	public String getdados() {
 		return dados;
 	}
